0aaaefac44e79d3b0889dab87abf0d038564efd5
Mike Hepburn <eformat@gmail.com>
2024-10-23 10:17:24 +1000

    🦩 initial commit 🦩

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # lora-fine-tune
+
+Fine tune a GPT Lora
+- https://dataman-ai.medium.com/fine-tune-a-gpt-lora-e9b72ad4ad3
+
+`python train.py`
+
+```bash
+{'loss': 2.1376, 'grad_norm': 0.6898021101951599, 'learning_rate': 4.000000000000001e-06, 'epoch': 1.26}       
+{'loss': 1.6841, 'grad_norm': 0.8817117214202881, 'learning_rate': 2.0000000000000003e-06, 'epoch': 1.27}      
+{'loss': 1.8884, 'grad_norm': 0.5318072438240051, 'learning_rate': 0.0, 'epoch': 1.28}                         
+{'train_runtime': 546.4885, 'train_samples_per_second': 5.856, 'train_steps_per_second': 0.366, 'train_loss': 1.9254487091302872, 'epoch': 1.28}
+100%|████████████████████████████████████████████████████████████████████████| 200/200 [09:06<00:00,  2.73s/it]
+```
+
+```bash
+tree outputs/
+outputs/
+├── adapter_config.json
+├── adapter_model.safetensors
+├── checkpoint-200
+│   ├── adapter_config.json
+│   ├── adapter_model.safetensors
+│   ├── optimizer.pt
+│   ├── README.md
+│   ├── rng_state.pth
+│   ├── scheduler.pt
+│   ├── trainer_state.json
+│   └── training_args.bin
+├── README.md
+└── training_args.bin
+
+1 directory, 12 files
+```
+
+`python infer.py`
+
+```bash
+Loading checkpoint shards: 100%|█████████████████████████████████████████████████| 2/2 [00:02<00:00,  1.21s/it]
+
+Two things are infinite:  the universe and human stupidity; and I'm not sure about the universe.  -Albert Einstein
+I'm not sure about the universe either.
+```
---
+++ train.py
@@ -0,0 +1,72 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+import torch
+import torch.nn as nn
+import bitsandbytes as bnb
+from transformers import AutoTokenizer, AutoConfig, AutoModelForCausalLM
+
+model = AutoModelForCausalLM.from_pretrained(
+    "facebook/opt-6.7b", 
+    load_in_8bit=True, 
+    device_map='auto',
+)
+
+tokenizer = AutoTokenizer.from_pretrained("facebook/opt-6.7b")
+
+from peft import prepare_model_for_kbit_training
+model = prepare_model_for_kbit_training(model)
+
+def print_trainable_parameters(model):
+    """
+    Prints the number of trainable parameters in the model.
+    """
+    trainable_params = 0
+    all_param = 0
+    for _, param in model.named_parameters():
+        all_param += param.numel()
+        if param.requires_grad:
+            trainable_params += param.numel()
+    print(
+        f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
+    )
+
+from peft import LoraConfig, get_peft_model 
+
+config = LoraConfig(
+    r=16,
+    lora_alpha=32,
+    target_modules=["q_proj", "v_proj"],
+    lora_dropout=0.05,
+    bias="none",
+    task_type="CAUSAL_LM"
+)
+
+model = get_peft_model(model, config)
+print_trainable_parameters(model)
+
+from datasets import load_dataset
+data = load_dataset("Abirate/english_quotes")
+data = data.map(lambda samples: tokenizer(samples['quote']), batched=True)
+
+import transformers
+
+trainer = transformers.Trainer(
+    model=model, 
+    train_dataset=data['train'],
+    args=transformers.TrainingArguments(
+        per_device_train_batch_size=4, 
+        gradient_accumulation_steps=4,
+        warmup_steps=100, 
+        max_steps=200, 
+        learning_rate=2e-4, 
+        fp16=True,
+        logging_steps=1, 
+        output_dir='outputs'
+    ),
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False)
+)
+
+model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
+
+trainer.train()
+trainer.save_model("outputs")
